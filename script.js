6d0d114df3b1e552b750eb7ffef68bd9b6674d73
ArthurVoit <avoitreder@gmail.com>
2025-03-20 12:57:15 -0300

    update - atividade 1

---
+++ script.js
@@ -0,0 +1,13 @@
+let numero = prompt("fala um numero")
+function verificarSinal(str){
+    if(str.include("-")){
+        console.log(str, "é negativo")
+    }
+    if(str != 0, str.include("-")){
+        console.log(str, "é zero")
+    }
+    else{
+        console.log(str, "é zero")
+    }
+}
+verificarSinal(numero);
